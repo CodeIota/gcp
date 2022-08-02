7fba701673e5e880450a1159aca358a006745ca2
CodeIota <101242602+CodeIota@users.noreply.github.com>
2022-08-01 20:32:54 -0400

    json to string

--- src/main/java/gcp_api/gcp/services/PotServices.java
+++ src/main/java/gcp_api/gcp/services/PotServices.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gcp_api.gcp.domain.ProductsPot;
@@ -48,9 +50,6 @@ public class PotServices {
         try (InputStream inputStream = new FileInputStream(new File("/home/rubendgomes/Documents/GitHub/gcp/src/main/resources/data/pot.json"))) {
             TypeReference<List<ProductsPot>> typeReference = new TypeReference<List<ProductsPot>> () {};
             List<ProductsPot> products = mapper.readValue(inputStream, typeReference);
-            TypeReference<String> typeReference2 = new TypeReference<String>() {};
-            String data = mapper.readValue(inputStream, typeReference2);
-            System.out.println(data);
 
             for (ProductsPot p: products) {
                 if (p.getProduct().equals(product)) {
@@ -67,12 +66,28 @@ public class PotServices {
                 }
             }
 
+            // System.out.println(data);
+
             mapper.writeValue(new File("/home/rubendgomes/Documents/GitHub/gcp/src/main/resources/data/pot.json"), products);
             // return "{\"message\":success}";
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public String jsonToString() throws StreamReadException, DatabindException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String data = "";
+        InputStream inputStream = new FileInputStream(new File("/home/rubendgomes/Documents/GitHub/gcp/src/main/resources/data/pot.json"));
+        TypeReference<List<ProductsPot>> typeReference = new TypeReference<List<ProductsPot>> () {};
+        List<ProductsPot> products = mapper.readValue(inputStream, typeReference);
+
+        for (ProductsPot p: products) {
+            data += mapper.writeValueAsString(p); 
+        }
+
+        return data;
     }
 
 
